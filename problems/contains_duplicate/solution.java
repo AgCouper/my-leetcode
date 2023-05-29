4dfb16bd393adfb3eb6dcb4b16251ca2a4403b13
Leonid Romanov <leonidrom@yandex-team.ru>
2023-05-29 18:15:18 +0000

    Sync LeetCode submission - Contains Duplicate (java)

---
+++ problems/contains_duplicate/solution.java
@@ -0,0 +1,17 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> vals = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int v = nums[i];
+            boolean isNew = vals.add(v);
+            if (!isNew) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
