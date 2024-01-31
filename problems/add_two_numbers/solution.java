a661187cc8dd18ee89798618fc0b20853c650736
Leonid Romanov <leonidrom@yandex-team.ru>
2024-01-31 10:02:37 +0000

    Sync LeetCode submission - Add Two Numbers (java)

--- problems/add_two_numbers/solution.java
+++ problems/add_two_numbers/solution.java
@@ -10,58 +10,34 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null;
-        ListNode tail = null;
-    
-        int carry = 0;
+        int n1, n2, c = 0;
+        ListNode h = null, t = null;
         while (l1 != null || l2 != null) {
-            int val = 0;
+            n1 = l1 == null? 0 : l1.val;
+            n2 = l2 == null? 0 : l2.val;
+            int s = n1 + n2 + c;
+            c = s / 10;
+            int n = s % 10;
+
+            var ln = new ListNode(n);
             
-            if (l1 == null) {
-                val = l2.val + carry;
-                carry = 0;
-                if (val >= 10) {
-                    val = val - 10;
-                    carry = 1;
-                }
-                
-                l2 = l2.next;
-            } else if (l2 == null) {
-                val = l1.val + carry;
-                carry = 0;
-                if (val >= 10) {
-                    val = val - 10;
-                    carry = 1;
-                }
-                
-                l1 = l1.next;                
+            l1 = l1 == null? null : l1.next;
+            l2 = l2 == null? null : l2.next;
+
+            if (h == null) {
+                h = t = ln;
             } else {
-                val = l1.val + l2.val + carry;
-                carry = 0;
-                if (val >= 10) {
-                    val = val - 10;
-                    carry = 1;
-                }
-                
-                l1 = l1.next;
-                l2 = l2.next;
-            }
-            
-            ListNode n = new ListNode(val, null);
-            if (head == null) {
-                head = n;
-                tail = n;
-            } else {
-                tail.next = n;
-                tail = n;
+                t.next = ln;
+                t = ln;
             }
         }
-        
-        if (carry != 0) {
-            ListNode n = new ListNode(1, null);
-            tail.next = n;
+
+        if (c != 0) {
+            var ln = new ListNode(c);
+            t.next = ln;
+            t = ln;
         }
-        
-        return head;
+
+        return h;   
     }
 }
