d27aa4c079b725258dbc7934b96703f23dc3dede
Leonid Romanov <leonidrom@yandex-team.ru>
2024-01-31 21:38:00 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- problems/longest_substring_without_repeating_characters/solution.java
+++ problems/longest_substring_without_repeating_characters/solution.java
@@ -1,29 +1,78 @@
+/*
+    Given a string s, find the length of the longest substring
+    without repeating characters.
+
+    Example 1:
+
+        Input: s = "abcabcbb"
+        Output: 3
+        Explanation: The answer is "abc", with the length of 3.
+*/
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.isEmpty()) {
+        /*
+            We use rolling window technique. On each iteration,
+            w[c] == true for all the characters within the window.
+            Because s consists of English letters, digits, symbols and spaces,
+            we can use array instead of map.
+        */
+        boolean[] w = new boolean[256];
+        Arrays.fill(w, false);
+
+        int l = 0, r = 0, len = s.length(), maxLen = 0;
+        if (len == 0) {
             return 0;
         }
 
-        int[] charPositions = new int[255];
-        Arrays.fill(charPositions, -1);
+        w[s.charAt(0)] = true;
 
-        int maxLen = 0;
-        int curLen = 0;
-        for (int pos = 0; pos < s.length(); ++pos) {
-            int charCode = s.charAt(pos);
-            int charPos = charPositions[charCode];
-            if (charPos == -1) {
-                charPositions[charCode] = pos;
-                curLen++;
+        // invariant: s[l:r] has no dups
+        while (true) {
+            int dupIdx = growWindow(w, r, s);
+            maxLen = Math.max(maxLen, dupIdx - l); 
+
+            if (dupIdx == len) {
+                break;
+            }
+
+            char dup = s.charAt(dupIdx);
+            l = shrinkWindow(w, l, dup, s);
+            r = dupIdx;
+        }
+        
+        return maxLen;
+    }
+
+    /**
+        Shrinks the window by moving left bound forward
+        until there are no dups in the window.
+    */
+    public int shrinkWindow(boolean[] w, int l, char dup, String s) {
+        while (s.charAt(l) != dup) {
+            w[s.charAt(l)] = false;
+            l++;
+        }
+
+        return l + 1;
+    }
+
+    /**
+        Grows the window by moving right bound forward
+        until there is a dup.
+        Returns the position of a rightmost dup character.
+    */
+    public int growWindow(boolean[] w, int r, String s) {
+        int res = s.length();
+        for (int i = r + 1; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (w[ch] != false) {
+                res = i;
+                break;               
             } else {
-                maxLen = Math.max(maxLen, curLen);
-                curLen = 0;
-                pos = charPos;
-                Arrays.fill(charPositions, -1);
+                w[ch] = true;
             }
         }
 
-        maxLen = Math.max(maxLen, curLen);
-        return maxLen;        
+        return res;
     }
 }
